9874bd02f61a2902f0c1f8f9a87296210ebcc615
Daniel Zambrano <74665548+ddzambranoa@users.noreply.github.com>
2022-04-15 17:58:33 -0500

    Add files via upload

---
+++ jsGame/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Cualquier persona que acuda a la consulta de un profesional de salud es un:",
+    answer: "Paciente",
+    options: [
+      "Médico",
+      "Odontólogo",
+      "Paciente",
+      "Enfermera"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Los servicios ofrecidos por profesionales de la salud son",
+    answer: "Prevención, tratamiento y bienestar fisico y mental",
+    options: [
+      "Prevención, tratamiento y Cuidado fisiológico",
+      "Prevención, hospitalización y bienestar fisico y mental",
+      "Cuidado, tratamiento y bienestar fisico y mental",
+      "Prevención, tratamiento y bienestar fisico y mental"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuál es una actividad asistencial?",
+    answer: "Sala de fisioterapia",
+    options: [
+      "Sala de fisioterapia",
+      "Consulta de Traumatología",
+      "Visitas médicas periódicas",
+      "Ninguna de las anteriores"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Qué incluye una actividad Edu-Comunicativas?",
+    answer: "Cuidadores",
+    options: [
+      "Cuidadores",
+      "Adiestrar a fisioterapeutas",
+      "Plan Post-Tratamiento",
+      "Ninguna de las anteriores"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Qué es la actividad administrativa y de gestión?",
+    answer: "Se encarga del agendamiento de citas, redacción de recetas y archivar la información y registros de pacientes",
+    options: [
+      "Se encarga del agendamiento de citas, redacción de recetas y archivar la información y registros de pacientes",
+      "Es la encargada de administrar la atención de cada paciente, preguntando al mismo su historial médico reciente",
+      "Se encarga de agendar y redactar los medicamentos a tomar por el paciente durante el tratamiento",
+      "Ninguna de las anteriores"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "¿Cuál no es un valor personal y profesional?",
+    answer: "Dependencia e imparcialidad",
+    options: [
+      "Integridad y diligencia",
+      "Competencia y desarrollo ",
+      "Dependencia e imparcialidad",
+      "legalidad"
+    ]
+  },
+  {
+    numb: 7,
+    question: "¿Qué incluye un historia clínica?",
+    answer: "Estado anterior y evolución",
+    options: [
+      "Información del individuo",
+      "Antecedentes",
+      "Estado anterior y evolución",
+      "Tratamientos recibidos"
+    ]
+  },
+  {
+    numb: 8,
+    question: "¿Cuál no es una norma de información?",
+    answer: "Todo el personal de salud tiene acceso a la información de la historia clínica sin obligación alguna",
+    options: [
+      "La información se registrará en una carpeta individual, rotulada con los apellidos y nombres del paciente.",
+      "El manejo de las historias clínicas puede ser realizado con personal disponible.",
+      "La información consignada en la historia clínica es propiedad del establecimiento de salud.",
+      "Todo el personal de salud tiene acceso a la información de la historia clínica sin obligación alguna"
+    ]
+  },
+  {
+    numb: 9,
+    question: "¿Cuál no es un recurso material en Fisioterapia?",
+    answer: "Uniformes Biomédicos",
+    options: [
+      "Infraestructura",
+      "Materiales de valoración ",
+      "Sistema de registro",
+      "Uniformes Biomédicos"
+    ]
+  },
+  {
+    numb: 10,
+    question: "¿Cuál no es parte de una historia clínica?",
+    answer: "Historia clínica pasiva",
+    options: [
+      "Historia clínica pasiva",
+      "Historia clínica activa",
+      "Historia clínica inactiva",
+      "Historia clínica"
+    ]
+  },
+];
